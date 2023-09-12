fe13b75b5627260efe588fe94da2a91e60eebaab
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-09-12 08:30:19 +0100

    Done with initial setup

---
+++ src/main/java/com/example/hng_internship_backend_stage_2_task/HngInternshipBackendStage2TaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.hng_internship_backend_stage_2_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngInternshipBackendStage2TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HngInternshipBackendStage2TaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hng_internship_backend_stage_2_task/HngInternshipBackendStage2TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hng_internship_backend_stage_2_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngInternshipBackendStage2TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
